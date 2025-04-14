e99eb73311b1304cb73bf0755fd5a9407662f948
Pinkey Prasad <pinkeyprasad2608@gmail.com>
2025-04-14 23:54:57 +0530

    This is first commit

---
+++ GuessNUm.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{   
+    int random , guess;
+    int numguess=0;
+    srand(time(NULL));
+
+    printf("\n\n******************Welcome to the world of Guessing Number******************\n");
+    random = rand() % 100 + 1 ;
+
+    
+    do {
+        printf("\nEnter the guessing number between(1 to 100): ");
+        scanf("%d",&guess);
+        if (guess > 100 || guess < 1) {
+            printf("Invalid input! Number should be between 1 and 100.\n");
+            printf("Program will now exit.\n");
+            return 0; // exit the program if input is out of range
+        }
+        numguess++;
+
+        if(guess < random) {
+            printf("Guess larger number\n ");
+        }
+        else if(guess>random) {
+            printf("Guess smaller number\n ");
+        }
+        else {
+            printf("\nCongratulation! YOU have Succcesfully guessed the number in %d attempt",numguess);
+        }
+    }
+    while( guess != random );
+    
+    printf("\nThanks for Playing");
+    printf("\nDeveloped by PINKEY PRASAD");
+    
+    return 0;
+}
